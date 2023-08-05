bad9d2c8f4cf33fc74dfc6510dc97f599e6178aa
user <you@example.com>
2023-08-05 10:10:07 +0800

    update

--- op-node/rollup/derive/batches.go
+++ op-node/rollup/derive/batches.go
@@ -61,10 +61,10 @@ func CheckBatch(cfg *rollup.Config, log log.Logger, l1Blocks []eth.L1BlockRef, l
 	}
 
 	// Filter out batches that were included too late.
-	if uint64(batch.Batch.EpochNum)+cfg.SeqWindowSize < batch.L1InclusionBlock.Number {
-		log.Warn("batch was included too late, sequence window expired")
-		return BatchDrop
-	}
+	// if uint64(batch.Batch.EpochNum)+cfg.SeqWindowSize < batch.L1InclusionBlock.Number {
+	// 	log.Warn("batch was included too late, sequence window expired")
+	// 	return BatchDrop
+	// }
 
 	// Check the L1 origin of the batch
 	batchOrigin := epoch
