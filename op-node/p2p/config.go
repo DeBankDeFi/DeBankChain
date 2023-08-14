31e68542d45d38c7cf3298b9d376d4e54b9c1c89
user <you@example.com>
2023-08-14 13:04:24 +0800

    add default bootnodes

--- op-node/p2p/config.go
+++ op-node/p2p/config.go
@@ -25,9 +25,27 @@ import (
 )
 
 var DefaultBootnodes = []*enode.Node{
-	enode.MustParse("enode://869d07b5932f17e8490990f75a3f94195e9504ddb6b85f7189e5a9c0a8fff8b00aecf6f3ac450ecba6cdabdb5858788a94bde2b613e0f2d82e9b395355f76d1a@34.65.67.101:0?discport=30305"),
-	enode.MustParse("enode://2d4e7e9d48f4dd4efe9342706dd1b0024681bd4c3300d021f86fc75eab7865d4e0cbec6fbc883f011cfd6a57423e7e2f6e104baad2b744c3cafaec6bc7dc92c1@34.65.43.171:0?discport=30305"),
-	enode.MustParse("enode://9d7a3efefe442351217e73b3a593bcb8efffb55b4807699972145324eab5e6b382152f8d24f6301baebbfb5ecd4127bd3faab2842c04cd432bdf50ba092f6645@34.65.109.126:0?discport=30305"),
+	// Node ID: 5845e1395d7e120912ba2588a8f0451f538caf98d69b9af3602d6f3ab8cc0759
+	// URLv4:   enode://e8154cad86b66c3b1cb9b2c875c263f0cdff8f98ddbfe89fc9516855d141e95593fa3cc5708da8d6c979ecc7a0a919fbc2eaefb7fa8f4198d9753d82151156c8@43.207.77.108:9222?discport=51787
+	// Record has sequence number 1691653398385 and 6 key/value pairs.
+	//   "id"        "v4"
+	//   "ip"        43.207.77.108
+	//   "opstack"   8496b07b00
+	//   "secp256k1" a102e8154cad86b66c3b1cb9b2c875c263f0cdff8f98ddbfe89fc9516855d141e955
+	//   "tcp"       9222
+	//   "udp"       51787
+	enode.MustParse("enr:-J64QA8c432-TY4uxlxQ4VXQOKWqPlh2g3fGZd_iTAE7PqmWaitTfZOyByHmux87_fqFlZhKtZ_sm-TPYxgIMaaGRBSGAYneZl9xgmlkgnY0gmlwhCvPTWyHb3BzdGFja4SWsHsAiXNlY3AyNTZrMaEC6BVMrYa2bDscubLIdcJj8M3_j5jdv-ifyVFoVdFB6VWDdGNwgiQGg3VkcILKSw"),
+
+	// 	Node ID: 0decb516278633804bfded1e1e5b5f684bbfa77f6fbd13cacfea6216db43c63a
+	// URLv4:   enode://43b5fa8dde7b216034ad97c2067ac21d4be62f3860b4d3956abd5baffecf48cedd36791c08e7697c2efec8c2012ce5ca8e919ca7d33c06652924737a0e8cc988@43.207.221.181:9222?discport=49268
+	// Record has sequence number 1691660727153 and 6 key/value pairs.
+	//   "id"        "v4"
+	//   "ip"        43.207.221.181
+	//   "opstack"   8496b07b00
+	//   "secp256k1" a10243b5fa8dde7b216034ad97c2067ac21d4be62f3860b4d3956abd5baffecf48ce
+	//   "tcp"       9222
+	//   "udp"       49268
+	enode.MustParse("enr:-J64QFI65ajctEyDMwkqoNt9tNAsyDFauQA45ugqOz9POiKrU--o2sHNpy3acA6muag_K0fEGRkmGTdkFwwMeMyf6_GGAYne1jNxgmlkgnY0gmlwhCvP3bWHb3BzdGFja4SWsHsAiXNlY3AyNTZrMaECQ7X6jd57IWA0rZfCBnrCHUvmLzhgtNOVar1br_7PSM6DdGNwgiQGg3VkcILAdA"),
 }
 
 type HostMetrics interface {
@@ -119,6 +137,9 @@ type Config struct {
 	Store ds.Batching
 
 	EnableReqRespSync bool
+
+	// SequencerAddr is used to verify whether the gossip blocks received from p2p application are signed by our sequencer
+	SequencerAddr string
 }
 
 func DefaultConnManager(conf *Config) (connmgr.ConnManager, error) {
